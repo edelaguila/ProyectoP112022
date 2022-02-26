8abfc714b0c1683e8cd4d6b7fcba2850245c2277
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-02-26 00:55:42 -0600

    Se ha agregado el area del cuadrado

--- CalculodeAreas.cpp
+++ CalculodeAreas.cpp
@@ -10,7 +10,8 @@ void mostrarArea(float area);
 
 const float Pi = 3.14154;
 
-int main(){
+int main()
+{
  char opcion;
  cout<<"Selecciona el area que deseas calcular"<<endl;
  cout<<" a) Circulo\n b) Cuadrado\n c) Rectangulo\n d) Triangulo\n"; cin>>opcion;
@@ -22,3 +23,39 @@ int main(){
  }
  return 0;
 }
+/*void mostrarArea(float area){   cout<<"El area es: "<<area<<endl; return;  }
+
+void areaDelCirculo(void){
+ float radio, area = 0;
+ cout<<"Ingresa el radio: "; cin>>radio;
+ area = pow(radio,2) * Pi;
+ mostrarArea(area);
+ return;
+}*/
+
+void areaDelCuadrado(void){
+ float lado, area = 0;
+ cout<<"Ingresa un lado: "; cin>>lado;
+ area = pow(lado,2);
+ mostrarArea(area);
+ return;
+}
+
+/*void areaDelRectangulo(void){
+ float base, altura, area = 0;
+ cout<<"Ingresa la base: "; cin>>base;
+ cout<<"Ingresa la altura: "; cin>>altura;
+ area = base * altura;
+ mostrarArea(area);
+ return;
+}
+
+void areaDelTriangulo(void){
+ float base, altura, area = 0;
+ cout<<"Ingresa la base: "; cin>>base;
+ cout<<"Ingresa la altura: "; cin>>altura;
+ area = (base * altura) / 2;
+ mostrarArea(area);
+ return;
+}*/
+
