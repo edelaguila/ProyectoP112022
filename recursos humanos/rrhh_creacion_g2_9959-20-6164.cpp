98459040b6706fe0e2067526e0a8b2f89d4ca92c
CarlosGonzalez20 <ll3224645@gmail.com>
2022-03-20 14:21:03 -0600

    adding menus

--- recursos humanos/rrhh_creacion_g2_9959-20-6164.cpp
+++ recursos humanos/rrhh_creacion_g2_9959-20-6164.cpp
@@ -1,182 +1,141 @@
-/*#include <iostream>
-using namespace std;
-
-class persona {
-public:
-    persona(string, int, int , int , string , int , string , string);
-    string strRegistrar();
-    string strModificar();
-    string strEliminar();
-    string strDatos();
-
-private:
-    string strNombre, strCorreoElectronico, strNivelEstudios, strDireccion;
-    int intNumeroTelefono, intEdad, intDocumentoIdentificacion, intSalario;
-
-};
-
-persona::persona(string strNombre, int intDocumentoIdentificacion, int intSalario, int intEdad, string strDireccion, int intNumeroTelefono, string strCorreoElectronico, string strNivelEstudios){
-    Nombre=strNombre;
-    DocumentoIdentificacion=intDocumentoIdentificacion;
-    Salario=intSalario;
-    Edad=intEdad;
-    Direccion=strDireccion;
-    NumeroTelefono=intNumeroTelefono;
-    CorreoElectronico=strCorreoElectronico;
-    NivelEstudios=strNivelEstudios;
-}
-
-string persona::strRegistrar(){
-cout << "Entrando al menu REGISTRAR" << endl;
-cout << "Nombre supervisor: " << Nombre << ", edad: " << Edad << endl;
-}
-
-string persona::strModificar(){
-cout << "Entrando al menu MODIFICAR" << endl;
-string Nombre=nuevoNombre;
-cout << "Ingrese el nuevo nombre del supervisor: "; cin << Nombre;
-}
-
-string persona::strEliminar(){
-cout << "Entrando al menu ELIMINAR" << endl;
-cout << "¿Que desea eliminar?" << endl;
-//falta terminar
-}
-
-string persona::strDatos(){
-cout << "Entrando al menu DATOS" << endl;
-cout << "Nombre: " << Nombre << " . Su ID es: " << DocumentoIdentificacion << " . Su numero de telefono es: " << NumeroTelefono << " . Su correo electronico es: " << CorreoElectronico << " . Su puesto es: " << Puesto;
-}
-
-class empleado:public persona {
-public:
-    empleado(string, int, int , int , string , int , string , string, string);
-    string datosEmpleado();
-private:
-    int salario;
-};
-
-contador::empleado(string strNombre, int intDocumentoIdentificacion, int intSalario, int intEdad, string strDireccion, int intNumeroTelefono, string strCorreoElectronico, string strNivelEstudios, string strPuesto):persona(strNombre,intDocumentoIdentificacion,intSalario,intEdad,strDireccion,intNumeroTelefono,strCorreoElectronico,strNivelEstudios){
-Puesto=strPuesto;
-}
-
-int main(){
-    persona primeraPersona("juan", 55569, 5300, 19, "45654", 45678, "5623@gmail.com", "universitario" );
-    cout << primeraPersona.strRegistrar();
-    cout << primeraPersona.strModificar("pedro");
-    cout << primeraPersona.strEliminar();
-    cout << primeraPersona.strDatos();
-    return 0;
-}
-*/
 #include <iostream>
 using namespace std;
 
 class persona {
 public:
-    void registrar(){
-        string nombre=" ", correo=" ", identificacion=" ";
-        int telefono, edad;
-        cout << "Entrando al menu --REGISTRAR--" << endl;
-        cout << "Ingrese el nombre de la persona a registrar: "; cin >> nombre; cout << endl;
-        cout << "Ingrese el numero de documento de identificacion de la persona: "; cin >> identificacion; cout << endl;
-        cout << "Ingrese la edad de la persona a registrar: "; cin >> edad; cout << endl;
-        cout << "Ingrese el correo de la persona a registrar: "; cin >> correo; cout << endl;
-        cout << "Ingrese el numero de telefono de la persona a registrar: "; cin >> telefono; cout << endl;
-        cout << "Ingrese la direccion de la persona a registrar: "; cin >> direccion; cout << endl;
-        cout << "Ingrese el nivel de estudios de la persona a registrar: "; cin >> estudios; cout << endl;
-        cout << "--Registro completado--" << endl;
-        //return main();
-    }
-    void modificar(){
-        string nuevoNombre=" ";
-        int nuevaEdad, nuevoTelefono,opcion=0;
-        cout << "Entrando al menu --MODIFICAR--" << endl;
-        cout << "Ingrese el numero correspondiente a lo que quiere modificar" << endl;
-        cout << "(1) Nombre\t" << endl;
-        cout << "(2) Edad\t" << endl;
-        cout << "(3) Telefono" << endl;
-        cout << "(4) Salir al menu principal" << endl;
-        switch (opcion){
-    case (1):
-        cout << "Ingrese el nuevo nombre: "; cin >> nuevoNombre; cout << endl;
-        //return main();
-        break;
-    case (2):
-        cout << "ingrese la nueva edad: "; cin >> nuevaEdad; cout << endl;
-        //return main();
-        break;
-    case (3):
-        cout << "Ingrese el nuevo numero de telefono: "; cin >> nuevoTelefono; cout << endl;
-        //return main();
-        break;
-    case (4):
-        cout << "Saliendo..." << endl;
-        //return main();
-        break;
-        }
-    }
-    void eliminar(){
-        cout << "Entrando al menu --ELIMINAR--" << endl;
-        int opcion=0;
-        cout << "Ingrese el numero correspondiente a lo que quiere modificar" << endl;
-        cout << "(1) Nombre\t" << endl;
-        cout << "(2) Edad\t" << endl;
-        cout << "(3) Telefono" << endl;
-        cout << "(4) Salir al menu principal" << endl;
-        //falta terminar
-        switch (opcion){
-    case (1):
-        cout << "Ingrese el nuevo nombre: \n"; //cin >> ;
-        //return main();
-        break;
-    case (2):
-        cout << "ingrese la nueva edad: \n"; //cin >> ;
-        //return main();
-        break;
-    case (3):
-        cout << "Ingrese el nuevo numero de telefono: \n"; //cin >> ;
-        //return main();
-        break;
-    case (4):
-        cout << "Saliendo..." << endl;
-        //return main();
-        break;
-        }
-    }
-
-    /*void mostrarDatos(){
-        cout << "Entrando al menu --MOSTRAR DATOS--" << endl;
-        if (nuevoNombre==" "){
-            cout << "El nombre es: " << nombre;
-        else
-        cout << "El nombre es: " << nuevoNombre;
-        }
-        cout << " . El numero de documento de identificacion es: " << identificacion << " . El correo electronico es: " << correo << " . El numero de telefono es: " << telefono << endl;
-        cout << "Saliendo..." << endl;
-        return main();*/
-    //}
+    void registrar();
+    void modificar();
+    void eliminar();
+    void mostrarDatos();
 private:
-    string direccion=" ", estudios=" ";
+    string nombre, documentoIdentificacion, direccion, edad, estudios, correo, telefono, puesto, sueldo;
+    string *modnombre, *moddocumentoIdentificacion, *moddireccion, *modedad, *modestudios, *modcorreo, *modtelefono, *modpuesto, *modsueldo;
 };
-int main(){
-    char p1;
-    int menu=0;
-    cout << "Bienvenido al programa de recursos humanos" << endl;
-    cout << "Elija el numero del menu al que quiere ingresar" << endl << "(1) Registrar\t" << "(2) Modificar\t" << "(3) Eliminar\t" << "(4) Mostrar Datos\t" << endl;
+
+void persona::registrar(){
+    cout << "\n\tEntrando al menu --REGISTRAR--" << endl;
+    cout << "\n Ingrese el nombre de la persona a registrar: "; cin >> nombre;
+    cout << " Ingrese el numero de documento de identificacion de la persona: "; cin >> documentoIdentificacion;
+    cout << " Ingrese la edad de la persona a registrar: "; cin >> edad;
+    cout << " Ingrese el correo de la persona a registrar: "; cin >> correo;
+    cout << " Ingrese el numero de telefono de la persona a registrar: "; cin >> telefono;
+    cout << " Ingrese la direccion de la persona a registrar: "; cin >> direccion;
+    cout << " Ingrese el nivel de estudios de la persona a registrar: "; cin >> estudios;
+    cout << " --Registro completado--" << endl;
+    //return main();
+}
+
+void persona::modificar(){
+    string nuevonombre, nuevodocumentoIdentificacion, nuevodireccion, nuevoedad, nuevoestudios, nuevocorreo, nuevotelefono, nuevopuesto, nuevosueldo;
+    int opcion;
+    cout << "\tEntrando al menu --MODIFICAR--" << endl;
+    cout << "\tIngrese el numero correspondiente a lo que quiere modificar\n" << endl;
+    cout << "\t\t[1] Nombre" << endl;
+    cout << "\t\t[2] Documento Identificacion" << endl;
+    cout << "\t\t[3] Direccion" << endl;
+    cout << "\t\t[4] Edad" << endl;
+    cout << "\t\t[5] Estudios" << endl;
+    cout << "\t\t[6] Correo Electronico" << endl;
+    cout << "\t\t[7] Numero de telefono" << endl;
+    cout << "\t\t[8] Puesto o cargo" << endl;
+    cout << "\t\t[9] Sueldo" << endl;
+    cout << "\t\t[0] Salir" << endl;
+    cin >> opcion;
+    switch (opcion){
+case 1:
+    cout << "\n\tIngrese el nuevo nombre: "; cin >> nuevonombre; cout << endl;
+    modnombre=&nombre;
+    *modnombre=nuevonombre;
+    cout << "\n\tMostrando el nombre cambiado: " << nombre << endl;
+    //return main();
+    break;
+case 2:
+    cout << "\n\tIngrese el nuevo Documento de Identificacion: "; cin >> nuevodocumentoIdentificacion; cout << endl;
+    moddocumentoIdentificacion=&documentoIdentificacion;
+    *moddocumentoIdentificacion=nuevodocumentoIdentificacion;
+    cout << "\n\tMostrando el nuevo Documento de Identificcacion: " << documentoIdentificacion << endl;
+    //return main();
+    break;
+case 3:
+    cout << "\n\tIngrese la nueva direccion: "; cin >> nuevodireccion; cout << endl;
+    moddireccion=&direccion;
+    *moddireccion=nuevodireccion;
+    cout << "\n\tMostrando nueva Direccion: " << direccion << endl;
+    //return main();
+    break;
+case 4:
+    cout << "\n\tIngrese la nueva edad: "; cin >> nuevoedad; cout << endl;
+    modedad=&edad;
+    *modedad=nuevoedad;
+    cout << "\n\tMostrando nueva edad: " << edad << endl;
+    //return main();
+    break;
+case 5:
+    cout << "\n\tIngrese el nuevo nivel de estudios: "; cin >> nuevoestudios; cout << endl;
+    modestudios=&estudios;
+    *modestudios=nuevoestudios;
+    cout << "\n\tMostrando el nuevo nivel de estudios: " << estudios << endl;
+    //return main();
+    break;
+case 6:
+    cout << "\n\tIngrese el nuevo Correo Electronico: "; cin >> nuevocorreo; cout << endl;
+    modcorreo=&correo;
+    *modcorreo=nuevocorreo;
+    cout << "\n\tMostrando nuevo Correo Electronico: " << correo << endl;
+    //return main();
+    break;
+case 7:
+    cout << "\n\tIngrese el nuevo Numero de telefono: "; cin >> nuevotelefono; cout << endl;
+    modtelefono=&telefono;
+    *modtelefono=nuevotelefono;
+    cout << "\n\tMostrando nuevo Numero de telefono: " << telefono << endl;
+    break;
+case 8:
+    cout << "\n\tIngrese el nuevo Puesto o cargo: "; cin >> nuevopuesto; cout << endl;
+    modpuesto=&puesto;
+    *modpuesto=nuevopuesto;
+    cout << "\n\tMostrando el nuevo Puesto o cargo: " << puesto << endl;
+    break;
+case 9:
+    cout << "\n\tIngrese el nuevo Salario: "; cin >> nuevosueldo; cout << endl;
+    modsueldo=&sueldo;
+    *modsueldo=nuevosueldo;
+    cout << "\n\tMostrando el nuevo Sueldo: " << sueldo << endl;
+    break;
+case 0:
+    cout << "\n\tSaliendo al menu principal\n";
+    //falta terminar
+    break;
+    }
+}
+
+void persona::eliminar(){
+    //falta terminar
+}
+
+void persona::mostrarDatos(){
+    //falta terminar
+}
+
+main(){
+    persona prueba;
+    int menu;
+    cout << "\n\tBienvenido al programa de --RECURSOS HUMANOS--" << endl;
+    cout << "\tElija el numero del menu al que quiere ingresar" << endl << "\t\t[1] Registrar\n" << "\t\t[2] Modificar\n" << "\t\t[3] Eliminar\n" << "\t\t[4] Mostrar Datos\n" << endl;
+    cin >> menu;
     switch (menu){
-    case '1':
-        persona p1.registrar();
+    case 1:
+        prueba.registrar();
         break;
-    case '2':
-        p1.modificar();
+    case 2:
+        prueba.modificar();
         break;
-    case '3':
-        p1.eliminar();
+    case 3:
+        prueba.eliminar();
         break;
-    case '4':
-        p1.mostrar();
+    case 4:
+        prueba.mostrarDatos();
         break;
         }
-        return main();
+        return 0;
     };
