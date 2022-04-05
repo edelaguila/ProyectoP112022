c08f82aa1c83d9ef4df5fdd2553ca56e5d000015
CarlosSandoval2003 <jawicho@outlook.com>
2022-04-05 14:53:28 -0600

    Fix-Menus

--- Proyecto_Nominas/G4/SistemaG4/main.cpp
+++ Proyecto_Nominas/G4/SistemaG4/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "Persona.h"
 #include "menus.h"
 using namespace std;
 
--- Proyecto_Nominas/G4/SistemaG4/src/Persona.cpp
+++ Proyecto_Nominas/G4/SistemaG4/src/Persona.cpp
@@ -1,14 +1,2 @@
 #include "Persona.h"
-#include <iostream>
-#include<conio.h>
-using namespace std;
-using std::string;
-#include <cstring>
-#include <fstream>
-#include <iomanip>
-#include <cstdlib>
 
-Persona::persona()
-{
-
-}
