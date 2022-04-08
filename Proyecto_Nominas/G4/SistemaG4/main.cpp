203f2022871256d6e98eef74a0ed8822d73284e2
CarlosSandoval2003 <jawicho@outlook.com>
2022-04-08 14:47:51 -0600

    Implementacion de agregar, imp, eliminar, buscar y consultar

--- Proyecto_Nominas/G4/SistemaG4/main.cpp
+++ Proyecto_Nominas/G4/SistemaG4/main.cpp
@@ -1,10 +1,298 @@
 #include <iostream>
-#include "menus.h"
+#include<conio.h>
+#include "Persona.h"
+using std::cout;
+using std::cerr;
+using std::cin;
+using std::endl;
+using std::ios;
+using std::left;
+using std::right;
+using std::fixed;
+using std::showpoint;
+
+#include <fstream>
+
+using std::ofstream;
+using std::ostream;
+using std::fstream;
+
+#include <iomanip>
+
+using std::setw;
+using std::setprecision;
+
+#include <cstdlib>
 using namespace std;
+
+//Declaracion de las funciones para abrir los menus
+void consultas();
+void gestionEmpleados();
+void contabilidad();
+void seguridad();
 
 main()
 {
-	menus menuP;
-	menuP.menuPrincipal();
-	return 0;
+//inicio menu principal
+int choice;
+	char x;
+	do
+    {
+	system("cls");
+
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t    SISTEMA RECURSOS HUMANOS    "<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t 1. Consultas"<<endl;
+	cout<<"\t\t\t 2. Contabilidad"<<endl;
+	cout<<"\t\t\t 3. Gestion Empleados"<<endl;
+	cout<<"\t\t\t 4. Seguridad"<<endl;
+	cout<<"\t\t\t 5. Exit"<<endl;
+
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\tOpcion a escoger:[1/2/3/4/5]"<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"Ingresa una Opcion: ";
+    cin>>choice;
+//Llamadas a los menus secundarios
+    switch(choice)
+    {
+    case 1:
+        consultas();
+		break;
+	case 2:
+        contabilidad();
+		break;
+	case 3:
+        gestionEmpleados();
+		break;
+	case 4:
+        seguridad();
+		break;
+	case 5:
+        break;
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+	}
+    }while(choice!= 5);
+    return 0;
+}
+//Declaracion menu consultas
+void consultas()
+{
+    //Creacion de un objeto de la clase Persona
+    Persona empleado;
+    fstream empleadosEntradaSalida = empleado.inicioArchivo();
+
+    int choice;
+	char x;
+	do
+    {
+	system("cls");
+
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t    CONSULTAS    "<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t 1. Consulta Empleados"<<endl;
+	cout<<"\t\t\t 2. Consulta Departamentos"<<endl;
+	cout<<"\t\t\t 3. Consulta Sueldos"<<endl;
+	cout<<"\t\t\t 4. Regresar"<<endl;
+
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\tOpcion a escoger:[1/2/3/4]"<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"Ingresa una Opcion: ";
+    cin>>choice;
+
+    switch(choice)
+    {
+    case 1:
+        empleado.consultarRegistro(empleadosEntradaSalida);
+		break;
+	case 2:
+        cout<<"Estamos trabajando en la Consulta de Departamentos"<<endl;
+		break;
+	case 3:
+        cout<<"Estamos trabajando en la Consulta de Sueldos"<<endl;
+		break;
+	case 4:
+	        cout<<"Presione Enter otra vez para confirmar"<<endl;
+		break;
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+	}
+	empleadosEntradaSalida.clear(); // reinicializar indicador de fin de archivo
+    getch();
+    }while(choice!= 4);
+}
+
+void contabilidad()
+{
+    int choice;
+	char x;
+	do
+    {
+	system("cls");
+
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t    CONTABILIDAD    "<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t 1. Consulta Nomina"<<endl;
+	cout<<"\t\t\t 2. Consulta Poliza"<<endl;
+	cout<<"\t\t\t 3. Consulta Planillas"<<endl;
+	cout<<"\t\t\t 4. Consulta Retenciones"<<endl;
+	cout<<"\t\t\t 5. Gestion Salarios"<<endl;
+	cout<<"\t\t\t 6. Transferencias"<<endl;
+	cout<<"\t\t\t 7. Regresar"<<endl;
+
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\tOpcion a escoger:[1/2/3/4/5/6/7]"<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"Ingresa una Opcion: ";
+    cin>>choice;
+
+    switch(choice)
+    {
+    case 1:
+        cout<<"Estamos trabajando en la Consulta de Nomina"<<endl;
+		break;
+	case 2:
+        cout<<"Estamos trabajando en la Consulta de Poliza"<<endl;
+		break;
+	case 3:
+        cout<<"Estamos trabajando en la Consulta de Planillas"<<endl;
+		break;
+	case 4:
+        cout<<"Estamos trabajando en la Consulta de Retenciones"<<endl;
+		break;
+	case 5:
+        cout<<"Estamos trabajando en la Gestion de Salarios"<<endl;
+		break;
+	case 6:
+        cout<<"Estamos trabajando en las Transferencias"<<endl;
+		break;
+	case 7:
+		break;
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+    }
+    }while(choice!= 7);
+}
+
+void gestionEmpleados()
+{
+    Persona empleado;
+
+    fstream empleadosEntradaSalida = empleado.inicioArchivo();
+    int choice;
+	char x;
+	do
+    {
+	system("cls");
+
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t    GESTION EMPLEADOS    "<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t 1. Consulta Empleados"<<endl;
+	cout<<"\t\t\t 2. Busqueda de Empleados"<<endl;
+	cout<<"\t\t\t 3. Agregar Empleados"<<endl;
+	cout<<"\t\t\t 4. Modificar Empleados"<<endl;
+	cout<<"\t\t\t 5. Eliminar Empleados"<<endl;
+	cout<<"\t\t\t 6. Imprimir Archivo Empleados"<<endl;
+	cout<<"\t\t\t 7. Regresar"<<endl;
+
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\tOpcion a escoger:[1/2/3/4/5/6/7]"<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"Ingresa una Opcion: ";
+    cin>>choice;
+
+    switch(choice)
+    {
+    case 1:
+        empleado.consultarRegistro(empleadosEntradaSalida);
+        cout << "" << endl;
+        cout << "Presiona enter para continuar" << endl;
+		break;
+	case 2:
+        empleado.busquedaRegistro(empleadosEntradaSalida);
+		break;
+	case 3:
+        empleado.nuevoRegistro(empleadosEntradaSalida);
+        cout << "" << endl;
+        cout<<"Empleado agregado satisfactoriamente"<<endl;
+        cout << "" << endl;
+        cout << "Presiona enter para continuar" << endl;
+		break;
+	case 4:
+        empleado.actualizarRegistro(empleadosEntradaSalida);
+        cout << "" << endl;
+        cout<<"Empleado modificado satisfactoriamente"<<endl;
+        cout << "" << endl;
+        cout << "Presiona enter para continuar" << endl;
+		break;
+	case 5:
+        empleado.eliminarRegistro(empleadosEntradaSalida);
+        cout << "" << endl;
+        cout<<"Empleado eliminado satisfactoriamente"<<endl;
+        cout << "" << endl;
+        cout << "Presiona enter para continuar" << endl;
+		break;
+    case 6:
+        empleado.imprimirRegistro(empleadosEntradaSalida);
+        cout << "" << endl;
+        cout<<"Informacion impresa satisfactoriamente"<<endl;
+        cout << "" << endl;
+        cout << "Presiona enter para continuar" << endl;
+		break;
+	case 7:
+	        cout<<"Presione Enter otra vez para confirmar"<<endl;
+		break;
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+	}
+	empleadosEntradaSalida.clear(); // reinicializar indicador de fin de archivo
+    getch();
+    }while(choice!= 7);
+}
+
+void seguridad()
+{
+    int choice;
+	char x;
+	do
+    {
+	system("cls");
+
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t    SEGURIDAD    "<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t 1. Cambio de Usuario"<<endl;
+	cout<<"\t\t\t 2. Cambio de Contraseña"<<endl;
+	cout<<"\t\t\t 3. Backup"<<endl;
+	cout<<"\t\t\t 4. Regresar"<<endl;
+
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\tOpcion a escoger:[1/2/3/4]"<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"Ingresa una Opcion: ";
+    cin>>choice;
+
+    switch(choice)
+    {
+    case 1:
+        cout<<"Estamos trabajando en el Cambio de Usuario"<<endl;
+		break;
+	case 2:
+        cout<<"Estamos trabajando en la Cambio de Contraseña"<<endl;
+		break;
+	case 3:
+        cout<<"Estamos trabajando en el Backup"<<endl;
+		break;
+	case 4:
+		break;
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+	}
+    }while(choice!= 4);
 }
