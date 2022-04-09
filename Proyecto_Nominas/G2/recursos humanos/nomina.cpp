1994e12b52275c75311003eeb33f527b0850be38
Diego Culajay <diegoofranciss@gmail.com>
2022-04-08 19:48:10 -0600

    Nomina

---
+++ Proyecto_Nominas/G2/recursos humanos/nomina.cpp
@@ -0,0 +1,11 @@
+#include "nomina.h"
+
+nomina::nomina()
+{
+    //ctor
+}
+
+nomina::~nomina()
+{
+    //dtor
+}
---
+++ Proyecto_Nominas/G2/recursos humanos/nomina.h
@@ -0,0 +1,16 @@
+#ifndef NOMINA_H
+#define NOMINA_H
+
+
+class nomina
+{
+    public:
+        nomina();
+        virtual ~nomina();
+
+    protected:
+
+    private:
+};
+
+#endif // NOMINA_H
