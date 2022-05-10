3c9e782187cd33d25750ea3f230aacc0aa7606cd
CarlosSandoval2003 <jawicho@outlook.com>
2022-05-09 20:22:27 -0600

    Clase Menus Agregada

---
+++ Proyecto_Nominas/G4/SistemaG4/include/menus.h
@@ -0,0 +1,20 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+
+class menus
+{
+    public:
+        menus();
+        virtual ~menus();
+        menuGeneral();
+        menuConsultas();
+        menuContabilidad();
+        menuEmpleados();
+        menuSeguridad();
+    protected:
+
+    private:
+};
+
+#endif // MENUS_H
