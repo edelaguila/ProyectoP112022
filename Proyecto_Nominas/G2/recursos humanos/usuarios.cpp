827aeb963bf224e7a1dc9c112a9a1455a64b92de
CarlosGonzalez20 <ll3224645@gmail.com>
2022-05-10 21:01:50 -0600

    agregando usuarios multiples

--- Proyecto_Nominas/G2/recursos humanos/usuarios.cpp
+++ Proyecto_Nominas/G2/recursos humanos/usuarios.cpp
@@ -1,4 +1,4 @@
-/*#include<iostream>
+#include<iostream>
 #include<fstream>
 #include<iomanip>
 using namespace std;
@@ -12,21 +12,4 @@ int main(){
     usuario.open("seguridad.dat",ios::app | ios::out | ios::binary);
     usuario<<std::left<<std::setw(5)<< numU <<std::left<<std::setw(50)<< usu <<std::left<<std::setw(50)<< contra << "\n";
     usuario.close();
-}*/
-#include <iostream>
-#include <string>
-#include <stdio.h>
-#include <time.h>
-const std::string currentDateTime() {
-    time_t now = time(0);
-    struct tm tstruct;
-    char buf[80];
-    tstruct = *localtime(&now);
-    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
-    return buf;
-}
-
-int main(){
-    std::cout << "currentDateTime()=" << currentDateTime() << std::endl;
-    getchar();
 }
