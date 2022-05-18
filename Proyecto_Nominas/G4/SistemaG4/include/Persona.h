98114da9043ad75e7e6ef10fcb2f6c120a4f1374
María José Véliz <mvelizo1@miumg.edu.gt>
2022-05-17 22:14:29 -0600

    Subiendo generación nomina

--- Proyecto_Nominas/G4/SistemaG4/include/Persona.h
+++ Proyecto_Nominas/G4/SistemaG4/include/Persona.h
@@ -32,7 +32,7 @@ class Persona {
 public:
 
    // constructor clase Persona
-   Persona( int = 0, string = "", string = "", string = "", string = "", string = "", string = "", string = "", string = "");
+   Persona( int = 0, string = "", string = "", int = 0, int = 0, string = "", string = "", int = 0, int = 0);
 
    // Funciones para obtener el ID
    void establecerId( int );
@@ -45,36 +45,40 @@ public:
    void establecerPrimerNombre( string );
    string obtenerPrimerNombre() const;
 
-   void establecerTelefono( string );
-   string obtenerTelefono() const;
+   void establecerPuesto( string );
+   string obtenerPuesto() const;
 
-   void establecerDpi( string );
-   string obtenerDpi() const;
+   void establecerSueldo( int );
+   int obtenerSueldo() const;
 
    void establecerEmail( string );
    string obtenerEmail() const;
 
-   void establecerDireccion( string );
-   string obtenerDireccion() const;
+   void establecerHoras( int );
+   int obtenerHoras() const;
 
-   void establecerGenero( string );
-   string obtenerGenero() const;
+   void establecerDias( int );
+   int obtenerDias() const;
 
-   void establecerDepartamento( string );
-   string obtenerDepartamento() const;
+   void establecerCuenta( int );
+   int obtenerCuenta() const;
 
 
    //Funciones para el tratado de archivos
     void imprimirRegistro( fstream& );
+    void imprimirRegistro2( fstream& );
     void actualizarRegistro( fstream& );
     void nuevoRegistro( fstream& );
     void eliminarRegistro( fstream& );
     void mostrarLinea( ostream&, const Persona & );
+    void mostrarLinea2( ostream&, const Persona & );
     int obtenerId( const char * const );
     void crearArchivoEmpleados();
     void consultarRegistro( fstream& );
+    void consultarRegistro2( fstream& );
     void busquedaRegistro(fstream&);
     void mostrarLineaPantalla( const Persona &);
+    void mostrarLineaPantalla2( const Persona &);
     fstream inicioArchivo();
 
 
@@ -85,12 +89,12 @@ private:
    int numeroId;
    char apellido[ 15 ];
    char primerNombre[ 10 ];
-   char telefono[ 9 ];
-   char dpi [ 14 ];
+   char puesto[ 15 ];
+   int  sueldo;
    char email[31];
-   char direccion[3];
-   char departamento[21];
-   char genero[2];
+   int hExtra;
+   int noCuenta;
+   int dias;
 
 };
 
