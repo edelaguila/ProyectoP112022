00b73ab52fffd2358374817a35f4c22e66ca1d3e
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-05-23 16:54:14 -0600

    consulta de poliza

--- Proyecto_Nominas/G4/SistemaG4/src/Poliza.cpp
+++ Proyecto_Nominas/G4/SistemaG4/src/Poliza.cpp
@@ -16,3 +16,49 @@ Poliza::~Poliza()
 {
     //dtor
 }
+void Poliza::menu()
+{
+    Bitacora control;
+    int choice;
+	char x;
+	do
+    {
+	system("cls");
+
+	cout<<"\t\t\t................................"<<endl;
+	cout<<"\t\t\t .  Consulta Poliza   ."<<endl;
+	cout<<"\t\t\t................................"<<endl;
+	cout<<"\t\t\t 1. Seguro de vida "<<endl;
+	cout<<"\t\t\t 2. Seguro de Salud "<<endl;
+	cout<<"\t\t\t 3. Seguro Familiar "<<endl;
+	cout<<"\t\t\t 4. Seguro Dental "<<endl;
+	cout<<"\t\t\t 5. Regresar"<<endl;
+
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t Opcion a escoger:[1/2/3/4/5]"<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"Ingresa tu Opcion: ";
+    cin>>choice;
+
+    switch(choice)
+    {
+    case 1:
+		cout <<"  - Muerte natural o por enfermedad "<<endl;
+		break;
+	case 2:
+		cout <<" - Atencion medica general "<<endl;
+		break;
+    case 3:
+		cout <<" - Asistencia medica para esposa e hijos"<<endl;
+		break;
+    case 4:
+		cout <<" - Consulta cada 6 meses "<<endl;
+	case 5:
+		cout <<"Presiona Enter para salir"<<endl;
+		break;
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+	}
+	getch();
+    }while(choice!= 5);
+}
