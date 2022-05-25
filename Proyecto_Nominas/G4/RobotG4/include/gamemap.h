9e56fdc82e75e6a57beec33ee99feb7c0d72384e
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-05-25 16:26:25 -0600

    Nivel4_agregado

--- Proyecto_Nominas/G4/RobotG4/include/gamemap.h
+++ Proyecto_Nominas/G4/RobotG4/include/gamemap.h
@@ -11,19 +11,24 @@ class gamemap
         mapcell cell [15][30];
         gamemap2();
         gamemap3();
+        gamemap4();
         void draw();
         void draw2();
         void draw3();
+        void draw4();
         // método que obtiene las coordenadas del jugador y actualiza el mapa
         bool setPlayerCell(int playerX, int playerY);
         bool setPlayerCell2(int playerX2, int playerY2);
         bool setPlayerCell3(int playerX3, int playerY3);
+        bool setPlayerCell4(int playerX4, int playerY4);
         void drawPortada();
         void drawPortada2();
         void drawPortada3();
+        void drawPortada4();
         void drawVictoria();
         void drawVictoria2();
         void drawVictoria3();
+        void drawVictoria4();
         virtual ~gamemap();
         int opcion = 0;
         bool isGameOver = false;
@@ -37,8 +42,11 @@ class gamemap
         void createMapToFile();
         void loadMapFromFile2();
         void loadMapFromFile3();
+        void loadMapFromFile4();
         void createMapToFile2();
         void createMapToFile3();
+        void createMapToFile4();
 };
 
 #endif // GAMEMAP_H
+
